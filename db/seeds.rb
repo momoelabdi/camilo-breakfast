8caf4432737438a7064a39255e43e92c7d57e177
Mohammed El Abdi <momo.elabdi@bluewin.ch>
2022-03-30 14:38:08 +0200

    show style

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-require 'faker'
+# require 'faker'
 
-Bakery.delete_all
+# Bakery.delete_all
 
-camilo = Bakery.create!(name: "chez camilo", discription: "specialiste en petit pains", street: "boulvard camilo", zip: 34 , city:"Monaco", latitude: 345, longitude: 345)
+camilo = Bakery.create!(name: "Chez camilo", discription: "specialiste en petit pains", street: "boulvard camilo", zip: 34 , city:"Monaco", latitude: 345, longitude: 345)
 
 Product.create!(name: "Tango", details: "Home made", price: 7, bakery: camilo)
 
-10.times do
-  Bakery.create!(name: Faker::Company.name, street: Faker::Address.street_name, zip: Faker::Address.zip, city: Faker::Address.city)
-end
+# 10.times do
+#   Bakery.create!(name: Faker::Company.name, street: Faker::Address.street_name, zip: Faker::Address.zip, city: Faker::Address.city)
+# end
