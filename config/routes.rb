e982e6cb2da526f46da61ff60a1dc5a3540c09f7
talita chapuis <talita.chapuis@gmail.com>
2022-03-30 13:00:41 +0200

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  authenticated :user do
+    root to: 'pages#home'
+  end
+  unauthenticated :user do
+    devise_scope :user do
+      get '/' => 'devise/sessions#new'
+    end
+  end
   devise_for :users
-  root to: 'pages#home'
   resources :bakeries, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
