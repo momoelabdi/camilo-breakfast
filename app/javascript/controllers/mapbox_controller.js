42afa8dbb9559c1637bdcb65719c740a5741b71a
Alexandre Camilo <alex.camilo6@hotmail.ch>
2022-04-04 14:40:36 +0200

    style 100% without button

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -16,6 +16,7 @@ export default class extends Controller {
       style: "mapbox://styles/mapbox/streets-v10"
     })
 
+
   //   const map = new mapboxgl.Map({
   //   container: 'map',
   // });
@@ -56,4 +57,5 @@ export default class extends Controller {
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
+
 }
